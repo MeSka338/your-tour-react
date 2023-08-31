850b04acee7a55d63d6df0e4ac080b9f2826e648
MeSka338 <halk49114@gmail.com>
2023-08-31 15:08:43 +0300

    Add TourForm: written css, html, add img #4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./sections/Header/Header";
 import SelectTour from "./sections/(2)SelectTour/SelectTour";
 import Reviews from "./sections/(4)Reviews/Reviews";
+import TourForm from "./sections/(3)TourForm/TourForm";
 
 function App() {
   return (
     <>
       <Header />
       <SelectTour />
+      <TourForm />
       <Reviews />
     </>
   );
