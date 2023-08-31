9eb0e3c778d7c0f565a61defae2d80fe50789ee9
MeSka338 <halk49114@gmail.com>
2023-08-31 14:57:19 +0300

    Add section Reviews: written html, css, add img #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+
 import Header from "./sections/Header/Header";
 import SelectTour from "./sections/(2)SelectTour/SelectTour";
+import Reviews from "./sections/(4)Reviews/Reviews";
+
 function App() {
   return (
     <>
       <Header />
       <SelectTour />
+      <Reviews />
     </>
   );
 }
