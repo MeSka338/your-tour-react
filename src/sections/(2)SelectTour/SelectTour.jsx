261d3f093e94f9fed28d66ea454cb775e9645c2b
MeSka338 <halk49114@gmail.com>
2023-09-01 18:16:59 +0300

    Add selecting tours: color change on click #8

--- src/sections/(2)SelectTour/SelectTour.jsx
+++ src/sections/(2)SelectTour/SelectTour.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./SelectTour.css";
 
+const menuArr = ["Популярные", "Авторские", "Походы", "Сплавы", "Велопрогулки"];
+
 const SelectTour = () => {
+  const menu = useRef();
+
+  const Change = (key) => {
+    const children = menu.current.children;
+    for (let i = 0; i < children.length; i++) {
+      children[i].classList.remove("active");
+    }
+    children[key].classList.add("active");
+  };
+
+  useEffect(() => {
+    menu.current.children[0].classList.add("active");
+  }, []);
+
   return (
     <section className="select-tour">
       <h2 className="select-tour__title h2 title">Выбери свой тур</h2>
-      <ul className="select-tour__menu">
-        <li className="select-tour-menu__item p-big active">
-          <a className="select-tour-menu__link">Популярные</a>
-        </li>
-        <li className="select-tour-menu__item p-big">
-          <a className="select-tour-menu__link">Авторские</a>
-        </li>
-        <li className="select-tour-menu__item p-big">
-          <a className="select-tour-menu__link">Походы</a>
-        </li>
-        <li className="select-tour-menu__item p-big">
-          <a className="select-tour-menu__link">Сплавы</a>
-        </li>
-        <li className="select-tour-menu__item p-big">
-          <a className="select-tour-menu__link">Велопрогулки</a>
-        </li>
+      <ul className="select-tour__menu" ref={menu}>
+        {menuArr.map((item, key) => {
+          return (
+            <li
+              className={"select-tour-menu__item p-big"}
+              key={key}
+              onClick={() => Change(key)}
+            >
+              <a className="select-tour-menu__link">{item}</a>
+            </li>
+          );
+        })}
       </ul>
       <div className="select-tour__options">
         <div className="card-container">
