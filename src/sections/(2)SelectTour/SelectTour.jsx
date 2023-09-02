7ed17d1e3ece0025f3a4cf2c2675cdc32e6e276a
MeSka338 <halk49114@gmail.com>
2023-09-02 14:18:26 +0300

    Refactor SelectTour: change to block maping #14

--- src/sections/(2)SelectTour/SelectTour.jsx
+++ src/sections/(2)SelectTour/SelectTour.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import Arr from "../../utils/array";
 import "./SelectTour.css";
 
 const menuArr = ["Популярные", "Авторские", "Походы", "Сплавы", "Велопрогулки"];
@@ -35,143 +36,33 @@ const SelectTour = () => {
         })}
       </ul>
       <div className="select-tour__options">
-        <div className="card-container">
-          <div className="card">
-            <div className="grad"></div>
-            <img
-              src="/img/select-tour/cardTourPhoto-1.jpg"
-              alt="1"
-              className="card-bg"
-            />
-            <div className="titles-container">
-              <h3 className="card__title h3">Путешествие в горы</h3>
-              <p className="card__subtitle p-small">от 80 000 руб</p>
-            </div>
+        {Arr(6, 1).map((item, key) => {
+          return (
+            <div className="card-container" key={key}>
+              <div className="card">
+                <div className="grad"></div>
+                <img
+                  src={`/img/select-tour/cardTourPhoto-${item}.jpg`}
+                  alt={`${item}`}
+                  className="card-bg"
+                />
+                <div className="titles-container">
+                  <h3 className="card__title h3">Путешествие в горы</h3>
+                  <p className="card__subtitle p-small">от 80 000 руб</p>
+                </div>
 
-            <a href="#" className="secondary-btn">
-              <p className="secondary-btn__title p-big">Подробнее</p>
-              <img
-                className="secondary-btn__icon"
-                src="/img/select-tour/arrow.svg"
-                alt="arrow"
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className="card-container">
-          <div className="card">
-            <div className="grad"></div>
-            <img
-              src="/img/select-tour/cardTourPhoto-2.jpg"
-              alt="1"
-              className="card-bg"
-            />
-            <div className="titles-container">
-              <h3 className="card__title h3">Путешествие в горы</h3>
-              <p className="card__subtitle p-small">от 80 000 руб</p>
+                <a href="#" className="secondary-btn">
+                  <p className="secondary-btn__title p-big">Подробнее</p>
+                  <img
+                    className="secondary-btn__icon"
+                    src="/img/select-tour/arrow.svg"
+                    alt="arrow"
+                  />
+                </a>
+              </div>
             </div>
-
-            <a href="#" className="secondary-btn">
-              <p className="secondary-btn__title p-big">Подробнее</p>
-              <img
-                className="secondary-btn__icon"
-                src="/img/select-tour/arrow.svg"
-                alt="arrow"
-              />
-            </a>
-          </div>
-        </div>
-        <div className="card-container">
-          <div className="card">
-            <div className="grad"></div>
-            <img
-              src="/img/select-tour/cardTourPhoto-3.jpg"
-              alt="1"
-              className="card-bg"
-            />
-            <div className="titles-container">
-              <h3 className="card__title h3">Путешествие в горы</h3>
-              <p className="card__subtitle p-small">от 80 000 руб</p>
-            </div>
-
-            <a href="#" className="secondary-btn">
-              <p className="secondary-btn__title p-big">Подробнее</p>
-              <img
-                className="secondary-btn__icon"
-                src="/img/select-tour/arrow.svg"
-                alt="arrow"
-              />
-            </a>
-          </div>
-        </div>
-        <div className="card-container">
-          <div className="card">
-            <div className="grad"></div>
-            <img
-              src="/img/select-tour/cardTourPhoto-4.jpg"
-              alt="1"
-              className="card-bg"
-            />
-
-            <div className="titles-container">
-              <h3 className="card__title h3">Путешествие в горы</h3>
-              <p className="card__subtitle p-small">от 80 000 руб</p>
-            </div>
-            <a href="#" className="secondary-btn">
-              <p className="secondary-btn__title p-big">Подробнее</p>
-              <img
-                className="secondary-btn__icon"
-                src="/img/select-tour/arrow.svg"
-                alt="arrow"
-              />
-            </a>
-          </div>
-        </div>
-        <div className="card-container">
-          <div className="card">
-            <div className="grad"></div>
-            <img
-              src="/img/select-tour/cardTourPhoto-5.jpg"
-              alt="1"
-              className="card-bg"
-            />
-            <div className="titles-container">
-              <h3 className="card__title h3">Путешествие в горы</h3>
-              <p className="card__subtitle p-small">от 80 000 руб</p>
-            </div>
-            <a href="#" className="secondary-btn">
-              <p className="secondary-btn__title p-big">Подробнее</p>
-              <img
-                className="secondary-btn__icon"
-                src="/img/select-tour/arrow.svg"
-                alt="arrow"
-              />
-            </a>
-          </div>
-        </div>
-        <div className="card-container">
-          <div className="card">
-            <div className="grad"></div>
-            <img
-              src="/img/select-tour/cardTourPhoto-6.jpg"
-              alt="1"
-              className="card-bg"
-            />
-            <div className="titles-container">
-              <h3 className="card__title h3">Путешествие в горы</h3>
-              <p className="card__subtitle p-small">от 80 000 руб</p>
-            </div>
-            <a href="#" className="secondary-btn">
-              <p className="secondary-btn__title p-big">Подробнее</p>
-              <img
-                className="secondary-btn__icon"
-                src="/img/select-tour/arrow.svg"
-                alt="arrow"
-              />
-            </a>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
